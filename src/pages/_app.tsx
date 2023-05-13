75ca07ea5d548101bf80245e5cf7faebaf53f281
Hyeonjong <nianelo4@gmail.com>
2023-05-13 23:27:16 +0900

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+import { RecoilRoot } from 'recoil';
 import { AppMain } from '@/components';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <AppMain>
-      <Component {...pageProps} />
-    </AppMain>
+    <RecoilRoot>
+      <AppMain>
+        <Component {...pageProps} />
+      </AppMain>
+    </RecoilRoot>
   );
 }
