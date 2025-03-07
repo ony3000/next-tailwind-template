7672d61ef6774d5b1b39909ed0589903277cb003
Hyeonjong <nianelo4@gmail.com>
2025-03-07 09:13:03 +0900

    Rewrite media query variant

--- src/components/BoxLink.tsx
+++ src/components/BoxLink.tsx
@@ -10,10 +10,10 @@ export default function BoxLink({ href, title, description }: BoxLinkProps) {
       href={href}
       className="group space-y-2 rounded-xl border border-zinc-500/0
         bg-gray-400/0 p-4 transition-colors duration-200
-        non-touchscreen:hover:border-zinc-500/[.15]
-        non-touchscreen:hover:bg-gray-400/10 dark:border-stone-300/0
-        dark:bg-stone-500/0 dark:non-touchscreen:hover:border-stone-300/[.15]
-        dark:non-touchscreen:hover:bg-stone-500/10"
+        non-touchscreen-hover:border-zinc-500/[.15]
+        non-touchscreen-hover:bg-gray-400/10 dark:border-stone-300/0
+        dark:bg-stone-500/0 dark:non-touchscreen-hover:border-stone-300/[.15]
+        dark:non-touchscreen-hover:bg-stone-500/10"
       target="_blank"
       rel="noopener noreferrer"
     >
@@ -21,7 +21,7 @@ export default function BoxLink({ href, title, description }: BoxLinkProps) {
         {title}{' '}
         <span
           className="inline-block transition-transform duration-200
-            motion-safe:non-touchscreen:group-hover:translate-x-1"
+            motion-safe:group-non-touchscreen-hover:translate-x-1"
         >
           &rarr;
         </span>
